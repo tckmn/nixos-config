053c7e2986da5b79d94ff6064f90dca44c3c55b6
Andy Tockman <andy@tck.mn>
2019-10-21 20:26:04 -0400

    fix network issues
    disabling ipv6 is a stopgap till i figure out why things break when i
    don't
    
    also, sometimes lo would just stop existing, which was a more pressing
    issue

--- cfg/network.nix
+++ cfg/network.nix
@@ -6,6 +6,7 @@
   networking.wireless.iwd.enable = true;
   networking.useNetworkd = true;
   networking.useDHCP = false;
+  networking.enableIPv6 = false;
 
   services.resolved.enable = true;
   services.openssh.enable = true;
@@ -15,6 +16,12 @@
   systemd.network.networks."99-main".dns = [ "1.1.1.1" "1.0.0.1" ];
   systemd.network.networks."99-main".DHCP = "yes";
   services.resolved.extraConfig = "[Resolve]\nDNS=1.1.1.1 1.0.0.1";
+
+  # have localhost
+  systemd.network.networks."01-lo" = {
+    matchConfig = { Name = "lo"; };
+    address = [ "127.0.0.1" ];
+  };
 
   # tell systemd not to wait until network connects to do things
   systemd.services.systemd-networkd-wait-online.enable = false;
