3407bc15d1bcc97529502e65180bd32d67a407cc
Andy Tockman <andy@tck.mn>
2021-01-06 16:46:46 -0600

    update pry

--- pkgs/pry/default.nix
+++ pkgs/pry/default.nix
@@ -1,13 +1,15 @@
-{ lib, bundlerApp }:
+{ lib, bundlerApp, bundlerUpdateScript }:
 
 bundlerApp {
   pname = "pry";
   gemdir = ./.;
   exes = [ "pry" ];
 
+  passthru.updateScript = bundlerUpdateScript "pry";
+
   meta = with lib; {
     description = "A Ruby runtime developer console and IRB alternative";
-    homepage    = https://pryrepl.org;
+    homepage    = "https://pryrepl.org";
     license     = licenses.mit;
     maintainers = [ maintainers.tckmn ];
     platforms   = platforms.unix;
