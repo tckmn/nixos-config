e149cbc43f3304c975459488eb25cb858a217c72
Andy Tockman <andy@tck.mn>
2021-06-08 16:57:27 -0400

    simplify network nonsense
    still don't understand anything about networks

--- cfg/network.nix
+++ cfg/network.nix
@@ -4,12 +4,11 @@
   networking.hostName = "llama";
   networking.hostId = "0e4ef622";
   networking.wireless.iwd.enable = true;
-  networking.useDHCP = false;
-  networking.interfaces.wlan0.useDHCP = true;
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
-  networking.hosts = { "127.0.0.1" = [ "vfr.tck.mn" ]; };
+  # networking.useDHCP = false;
+  # networking.interfaces.wlan0.useDHCP = true;
+  # networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking.hosts = { "127.0.0.1" = [ "localhost.tck.mn" ]; };
   networking.firewall.enable = false;
-  # networking.enableIPv6 = false;
 
   services.openssh.enable = true;
   services.printing.enable = true;
