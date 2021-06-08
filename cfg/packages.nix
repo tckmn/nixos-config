3560f793363943cf9104c82b9689b2d1e001e4eb
Andy Tockman <andy@tck.mn>
2021-06-08 17:02:42 -0400

    fd is actually useful
    many of these are silly but this one is good

--- cfg/packages.nix
+++ cfg/packages.nix
@@ -51,7 +51,7 @@
     " tools     " universal-ctags google-cloud-sdk
 
     ### UTILITIES
-    " files     " renameutils binutils moreutils file xdg-user-dirs xxd ripgrep
+    " files     " renameutils binutils moreutils file xdg-user-dirs xxd ripgrep fd
     " compress  " zip unzip p7zip
     " documents " djvu2pdf pandoc pdftk poppler_utils cmark
     " sys info  " htop acpi tlp sysstat psmisc light
